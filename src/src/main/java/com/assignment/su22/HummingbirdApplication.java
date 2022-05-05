1f45eb39665cb24ee62fff28d73c479fe7a8d3f9
Gleb Son <57506521+glebs0n1@users.noreply.github.com>
2022-05-05 18:16:01 +0300

    Add files via upload

---
+++ src/src/main/java/com/assignment/su22/HummingbirdApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.su22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HummingbirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HummingbirdApplication.class, args);
+	}
+
+}
---
+++ src/src/main/java/com/assignment/su22/game/impl/HummingbirdGameImpl.java
@@ -0,0 +1,19 @@
+package com.assignment.su22.game.impl;
+
+import java.io.IOException;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+import com.assignment.su22.game.HummingbirdGame;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class HummingbirdGameImpl implements HummingbirdGame {
+
+	@Override
+	public int hummingbirdGame(Resource resource) throws IOException {
+		//		TODO start your journey here
+
+		return -1;
+	}
+}
